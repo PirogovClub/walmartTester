144e39c2ee581669d22f2d6e2684a2b2216184d5
pirog <pirog@DESKTOP-UVRI864>
2019-01-25 22:13:44 +0300

    put parralel

--- src/test/java/club/pirogov/walmartclicker/TestParralel.java
+++ src/test/java/club/pirogov/walmartclicker/TestParralel.java
@@ -22,7 +22,7 @@ public class TestParralel {
 	//	testPutProductToBacket.openUrl("https://ifconfig.co/");
 		testPutProductToBacket.quitBrowser();
 	}
-	// @Test
+	 @Test
 	//@RepeatedTest(1)
 	public void runTest2() {
 		TestPutProductToBacket testPutProductToBacket = new TestPutProductToBacket();
@@ -36,7 +36,7 @@ public class TestParralel {
 	//	testPutProductToBacket.openUrl("https://ifconfig.co/");
 		testPutProductToBacket.quitBrowser();
 	}
-	//@Test
+	@Test
 	//@RepeatedTest(1)
 	public void runTest4() {
 		TestPutProductToBacket testPutProductToBacket = new TestPutProductToBacket();
